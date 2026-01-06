021f569bc71579b756045fe0abf42cb56ccea9bd
Jit-6 <jitdhank4@gmail.com>
2026-01-06 08:14:26 +0530

    Solution updated
    Removed redundant comments for TreeNode structure.

--- LC_1161_Maximum_Level_Sum_of_a_Binary_Tree.cpp
+++ LC_1161_Maximum_Level_Sum_of_a_Binary_Tree.cpp
@@ -6,18 +6,6 @@
 // SC: O(w) where w is the maximum width of the binary tree
 // ****************************************************************
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
  /**
  * Definition for a binary tree node.
  * struct TreeNode {
